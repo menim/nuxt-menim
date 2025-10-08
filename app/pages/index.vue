9099b22f3c4ef61b29430fdc6c78b89ba84f5bc5
Anatoliy <menim@users.noreply.github.com>
2025-10-08 08:12:54 +0300

    add landing page (#52)
    Co-authored-by: anatoliy <anatoliy.lipunov@g5.com>

---
+++ app/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hero bg-base-300 container mx-auto mt-4">
+    <div class="hero-content text-center min-h-96 ">
+      <div class="max-w-md">
+        <h1 class="text-4XL font-bold">
+          NUXT MENIM TRAVEL LOG
+        </h1>
+        <p class="py-6">
+          Keep track of your travels and adventures with Menim Travel Log. Log your trips, share your experiences, and explore new destinations all in one place.
+        </p>
+        <button class="btn btn-accent">
+          Sign In With Gith ub <Icon name="tabler:brand-github" size="24" />
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
