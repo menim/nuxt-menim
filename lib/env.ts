27977fdcc54aaf0b58949d002355fb1029b7d560
anatoliy <anatoliy.lipunov@g5.com>
2025-11-24 19:46:14 +0200

    :sparkles: setub db

--- lib/env.ts
+++ lib/env.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
-import tryParseEnv from "./try-parse-env.js";
+import tryParseEnv from "./try-parse-env";
 
 const EnvSchema = z.object({
   NODE_ENV: z.string(),
+  TURSO_DATABASE_URL: z.string(),
+  TURSO_AUTH_TOKEN: z.string(),
 });
 
 export type EnvSchema = z.infer<typeof EnvSchema>;
