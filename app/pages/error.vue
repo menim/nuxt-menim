8745f1f751abe9dd377e4c8fc77da74b9a1d110c
anatoliy <anatoliy.lipunov@g5.com>
2025-11-26 15:24:50 +0200

    add github oauth

---
+++ app/pages/error.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute();
+const error = route.query.error || "Unknown error occurred";
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col justify-center items-center">
+    <div role="alert" class="alert alert-error min-w-1/2 text-center  ">
+      <span>Oh! No ! {{ error }}</span>
+    </div>
+    <NuxtLink to="/" class="btn btn-primary mt-4 ">
+      Home
+      <Icon name="tabler:arrow-left" size="32 " />
+    </NuxtLink>
+  </div>
+</template>
--- lib/auth.ts
+++ lib/auth.ts
@@ -2,9 +2,17 @@ import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 import db from "./db/index";
+import env from "./env";
+
 // your drizzle instance
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
-    provider: "sqlite", // or "mysql", "sqlite"
+    provider: "sqlite",
   }),
+  socialProviders: {
+    github: {
+      clientId: env.AUTH_GITHUB_CLIENT_ID,
+      clientSecret: env.AUTH_GITHUB_CLIENT_SECRET,
+    },
+  },
 });
