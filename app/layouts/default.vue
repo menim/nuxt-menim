94c91bd88677d2db0cfb6da318425df54dda2cfd
Anatoliy <menim@users.noreply.github.com>
2025-12-01 17:50:34 +0200

    add session is provided by ssr (#60)
    Co-authored-by: anatoliy <anatoliy.lipunov@g5.com>

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,8 @@
+<script setup lang="ts">
+const authStore = useAuthStore();
+await authStore.init();
+</script>
+
 <template>
   <div class="flex flex-col min-h-screen">
     <AppNavBar />
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,11 +1,22 @@
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { createAuthMiddleware } from "better-auth/plugins";
 
 import db from "./db/index";
 import env from "./env";
 
 // your drizzle instance
 export const auth = betterAuth({
+  hooks: {
+    after: createAuthMiddleware(async (ctx) => {
+      if (ctx.path === "/get-session") {
+        if (!ctx.context.session) {
+          return ctx.json({ user: null, session: null });
+        }
+        return ctx.json(ctx.context.session);
+      }
+    }),
+  },
   database: drizzleAdapter(db, {
     provider: "sqlite",
   }),
