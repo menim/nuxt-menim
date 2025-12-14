518cc43f0375c345725c4361bf89e8db0168be08
anatoliy <anatoliy.lipunov@g5.com>
2025-12-14 17:21:05 +0200

    Add sidebar component

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -6,7 +6,7 @@ await authStore.init();
 <template>
   <div class="flex flex-col min-h-screen">
     <AppNavBar />
-    <main>
+    <main class="flex-1 flex flex-col">
       <slot />
     </main>
   </div>
