c3ed8efe7ff1f7a70a077ab9fdd6f64b65ce9265
Anatoliy <menim@users.noreply.github.com>
2025-12-17 11:06:44 +0200

    Add logic to create new locations with detailed info (#62)
    Co-authored-by: anatoliy <anatoliy.lipunov@g5.com>

---
+++ app/pages/dashboard/add.vue
@@ -0,0 +1,124 @@
+<script setup lang="ts">
+import type { FetchError } from "ofetch";
+
+import { toTypedSchema } from "@vee-validate/zod";
+import { InsertLocation } from "~~/lib/db/schema";
+
+const { $csrfFetch } = useNuxtApp();
+const router = useRouter();
+const submitError = ref("");
+const loading = ref(false);
+const submitted = ref(false);
+const { handleSubmit, errors, meta, setErrors } = useForm({
+  validationSchema: toTypedSchema(InsertLocation),
+});
+
+const onSubmit = handleSubmit(async (values) => {
+  try {
+    submitError.value = "";
+    loading.value = true;
+    await $csrfFetch("/api/locations", {
+      method: "post",
+      body: values,
+    });
+    submitted.value = true;
+    navigateTo("/dashboard");
+  }
+  catch (e) {
+    const error = e as FetchError;
+    if (error.data?.data) {
+      setErrors(error.data?.data);
+    }
+    submitError.value = error.statusMessage || "An unknown error occurred.";
+    console.log(submitError.value, "#CURRENT LOG");
+  }
+  loading.value = false;
+});
+
+onBeforeRouteLeave(() => {
+  if (!submitted.value && meta.value.dirty) {
+    // eslint-disable-next-line no-alert
+    const confirm = window.confirm(
+      "You have unsaved changes. Are you sure you want to leave?",
+    );
+    if (!confirm) {
+      return false;
+    }
+  }
+  return true;
+});
+</script>
+
+<template>
+  <div class="container max-w-md mx-auto">
+    <div class="my-4">
+      <h1 class="text-lg">
+        Add Location
+      </h1>
+      <p class="text-sm">
+        A location is a place you have visited or want to visit. You can add a location by entering its name and coordinates.
+      </p>
+    </div>
+    <div v-if="submitError.value" role="alert" class="alert alert-error">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        class="stroke-info h-6 w-6 shrink-0"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+        />
+      </svg>
+      <span>{{ submitError }}</span>
+    </div>
+    <form action="" class="flex flex-col gap-2" @submit.prevent="onSubmit">
+      <AppFormField
+        label="Name"
+        name="name"
+        type="text"
+        :error="errors.name"
+        :disabled="loading"
+      />
+      <AppFormField
+        label="Description"
+        name="description"
+        type="textarea"
+        :disabled="loading"
+      />
+      <AppFormField
+        label="Latitude"
+        name="lat"
+        type="number"
+        :error="errors.lat"
+        :disabled="loading"
+      />
+      <AppFormField
+        label="Longtitude"
+        name="long"
+        type="number"
+        :error="errors.long"
+        :disabled="loading"
+      />
+      <div class="flex justify-end gap-2">
+        <button
+          :disabled="loading"
+          type="button"
+          class="btn btn-outline"
+          @click="router.back()"
+        >
+          <Icon name="tabler:arrow-left" size="24" />
+          Cancel
+        </button>
+        <button :disabled="loading" type="submit" class="btn btn-primary">
+          Add
+          <span v-if="loading" class="loading loading-spinner loading-sm" />
+          <Icon v-else name="tabler:circle-plus-filled" size="24" />
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
