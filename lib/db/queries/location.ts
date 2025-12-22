79d32ff63593fd246a7590e1970f1b7230853ab8
anatoliy <anatoliy.lipunov@g5.com>
2025-12-22 13:55:04 +0200

    consolidate db queries into seperate place

---
+++ lib/db/queries/location.ts
@@ -0,0 +1,48 @@
+import type { InsertLocation } from "~~/lib/db/schema";
+
+import { location } from "~~/lib/db/schema";
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+
+import db from "../index";
+
+const nanoid = customAlphabet("abcdefghijklmnopqrstuvwxyz", 5);
+
+export async function findLocationByName(existing: InsertLocation, userId: number) {
+  return db.query.location.findFirst({
+    where: and(
+      eq(location.name, existing.name),
+      eq(location.userId, userId),
+    ),
+  });
+}
+
+export async function findLocationBySlug(slug: string) {
+  return await db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  });
+}
+
+export async function findUniqueSlug(slug: string) {
+  let existing = !!(await findLocationBySlug(slug));
+
+  while (existing) {
+    const id = nanoid();
+    const idSlug = `${slug}-${id}`;
+    existing = !!(await findLocationBySlug(idSlug));
+    if (!existing) {
+      return idSlug;
+    }
+  }
+  return slug;
+}
+
+export async function insertLocation(insertable: InsertLocation, slug: string, userId: number) {
+  const [created] = await db.insert(location).values({
+    ...insertable,
+    slug,
+    userId,
+  }).returning();
+
+  return created;
+}
